de70e0e05b11debf6a8246f1145f7d4bae7a0dff
tcrenshaw-ET <tcrenshaw@ia.ua.edu>
2022-09-26 11:15:47 -0500

    updated and comented shell script

--- flip_all_bds_eztv.sh
+++ flip_all_bds_eztv.sh
@@ -2,10 +2,11 @@
 
 ## assumes we're in the right directory with the python script
 
-python3 ./poe_flipper.py -t eztv -s 172.25.1.30
-python3 ./poe_flipper.py -t eztv -s 172.25.1.31
-python3 ./poe_flipper.py -t eztv -s 172.25.1.32
-python3 ./poe_flipper.py -t eztv -s 172.25.1.33
-python3 ./poe_flipper.py -t eztv -s 172.25.1.34
-python3 ./poe_flipper.py -t eztv -s 172.25.1.35
-python3 ./poe_flipper.py -t eztv -s 172.25.1.29
+python3 ./poe_flipper.py -t eztv -s 172.25.1.30 # N133
+python3 ./poe_flipper.py -t eztv -s 172.25.1.31 # N217B
+python3 ./poe_flipper.py -t eztv -s 172.25.1.32 # N514
+python3 ./poe_flipper.py -t eztv -s 172.25.1.33 # E554
+python3 ./poe_flipper.py -t eztv -s 172.25.1.34 # W605
+python3 ./poe_flipper.py -t eztv -s 172.25.1.35 # W615
+python3 ./poe_flipper.py -t eztv -s 172.25.1.29 # E101
+python3 ./poe_flipper.py -t eztv -s 172.25.1.28 # N512
